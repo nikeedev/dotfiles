d3d2270378aa07481f570f3e9f246070c53cae5a
Niktia Goncarenko <nikita.gon.dev@gmail.com>
2025-12-12 22:30:13 +0100

    'updates'

--- nvim/init.lua
+++ nvim/init.lua
@@ -103,6 +103,17 @@ require("lazy").setup({
             vim.g.vimtex_view_method = "zathura"
         end
     },
+
+    {
+        'MeanderingProgrammer/render-markdown.nvim',
+        dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' },            -- if you use the mini.nvim suite
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+        ---@module 'render-markdown'
+        ---@type render.md.UserConfig
+        opts = {},
+    }
+
 })
 
 require("config")
